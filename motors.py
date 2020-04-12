dd873eadc29826445944547b181fdce9ee10a158
Patrick Li <patrick@Patricks-MacBook-Pro.local>
2020-04-12 15:20:52 -0400

    Change 03 to 3

--- motors.py
+++ motors.py
@@ -2,18 +2,18 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(03, GPIO.out)
-pwm=GPIO.PWM(03, 50)
+GPIO.setup(3, GPIO.out)
+pwm=GPIO.PWM(3, 50)
 pwm.start(0)
 
 servo_angle = 90
 
 def SetAngle(angle):
     duty = angle / 18 + 2
-    GPIO.output(03, True)
+    GPIO.output(3, True)
 	pwm.ChangeDutyCycle(duty)
 	sleep(1)
-	GPIO.output(03, False)
+	GPIO.output(3, False)
 	pwm.ChangeDutyCycle(0)
 
 def fwd():
