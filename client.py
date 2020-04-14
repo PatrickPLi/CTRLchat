905091eb9e3d677a4aba7d5e7992184f7fb38163
Patrick Li <patrick@Patricks-MacBook-Pro.local>
2020-04-14 15:19:38 -0400

    Added steering wheel and pedals clipart

--- client.py
+++ client.py
@@ -3,12 +3,12 @@ import time
 import keyboard
 
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon, QColor, QPalette
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QWidget, QShortcut, QStackedLayout, QLabel, QApplication, QHBoxLayout, QDial, QVBoxLayout, QHBoxLayout, QSlider, QPushButton, QMainWindow
+from PyQt5.QtGui import QIcon, QColor, QPalette, QKeySequence, QPixmap, QTransform
+from PyQt5 import QtCore
 
-throttle = 0
-steering = 50
+throttle_val = 0
+steering_val = 50
 
 class Color(QWidget):
 
@@ -29,6 +29,9 @@ class MainWindow(QMainWindow):
 
         vbox = QVBoxLayout()
         hbox = QHBoxLayout()
+        hbox2 = QHBoxLayout()
+
+        wheel_stack = QStackedLayout()
 
         widget = QWidget()
 
@@ -53,21 +56,48 @@ class MainWindow(QMainWindow):
         # right_btn.move(96, 64)
         right_btn.clicked.connect(right)
 
-        self.throttle = QProgressBar(widget)
+        self.throttle = QSlider(widget)
         self.throttle.setOrientation(0x2)
         self.throttle.setProperty("value", 0)
+
+        ss = "::handle {image: url(pedal.svg)}"
+        self.throttle.setStyleSheet(ss)
         self.steering = QDial(widget)
         self.steering.setMinimum(0)
         self.steering.setMaximum(100)
         self.steering.setProperty("value", 50)
+        self.steering.valueChanged.connect(change_steering)
+        self.throttle.valueChanged.connect(change_throttle)
+
+        self.steering_wheel = QPixmap('steering-wheel.svg')
+        self.wheel_label = QLabel(self)
+        self.wheel_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.wheel_label.setMinimumSize(400,400)
+        self.wheel_label.setPixmap(self.steering_wheel)
+
+        self.accel_shortcut = QShortcut(QKeySequence("w"), self)
+        self.accel_shortcut.activated.connect(accelerate)
+
+        self.decel_shortcut = QShortcut(QKeySequence("s"), self)
+        self.decel_shortcut.activated.connect(decelerate)
+
+        self.left_shortcut = QShortcut(QKeySequence("a"), self)
+        self.left_shortcut.activated.connect(left)
+
+        self.right_shortcut = QShortcut(QKeySequence("d"), self)
+        self.right_shortcut.activated.connect(right)
+
 
         hbox.addWidget(left_btn)
         hbox.addWidget(decelerate_btn)
         hbox.addWidget(right_btn)
         vbox.addWidget(accelerate_btn)
         vbox.addLayout(hbox)
-        vbox.addWidget(self.throttle)
-        vbox.addWidget(self.steering)
+        hbox2.addWidget(self.throttle)
+        wheel_stack.addWidget(self.wheel_label)
+        wheel_stack.addWidget(self.steering)
+        hbox2.addLayout(wheel_stack)
+        vbox.addLayout(hbox2)
         
         main_widget = QWidget()
         main_widget.setLayout(vbox)
@@ -78,43 +108,55 @@ class MainWindow(QMainWindow):
 
     def set_steering(self, sval):
         self.steering.setProperty("value", sval)
+        transform = QTransform()
+        transform.rotate(sval*2-100)
+        steering_wheel = QPixmap('steering-wheel.svg')
+        self.steering_wheel = steering_wheel.transformed(transform)
+        self.wheel_label.setPixmap(self.steering_wheel)
+
+def change_steering():
+    global steering_val
+    steering_val = window.steering.value()
+    client.publish('steering', payload=steering_val)
+
+def change_throttle():
+    global throttle_val
+    throttle_val = window.throttle.value()
+    client.publish('throttle', payload=throttle_val)
 
 def accelerate():
     print("w")
-    global throttle
-    throttle += 10
-    if throttle > 99:
-        throttle = 100
-    client.publish('throttle', payload=throttle)
-    window.set_throttle(throttle)
+    global throttle_val
+    throttle_val += 5
+    if throttle_val > 99:
+        throttle_val = 100
+    window.set_throttle(throttle_val)
 
 def decelerate():
     print("s")
-    global throttle
-    throttle -= 10
-    if throttle < 1:
-        throttle = 0
-    client.publish('throttle', payload=throttle)
-    window.set_throttle(throttle)
+    global throttle_val
+    throttle_val -= 5
+    if throttle_val < 1:
+        throttle_val = 0
+    client.publish('throttle', payload=throttle_val)
+    window.set_throttle(throttle_val)
 
 def left():
     print("a")
-    global steering
-    steering -= 10
-    if steering < 1:
-        steering = 0
-    client.publish('steering', payload=steering)
-    window.set_steering(steering)
+    global steering_val
+    steering_val -= 5
+    if steering_val < 1:
+        steering_val = 0
+    window.set_steering(steering_val)
 
 
 def right():
     print("d")
-    global steering
-    steering += 10
-    if steering > 99:
-        steering = 100
-    client.publish('steering', payload=steering)
-    window.set_steering(steering)
+    global steering_val
+    steering_val += 5
+    if steering_val > 99:
+        steering_val = 100
+    window.set_steering(steering_val)
 
 
 client = initialize()
